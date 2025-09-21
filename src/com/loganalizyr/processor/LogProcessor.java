04ceadcc40bd1ad3813ab110c6fb91162b3d4e5e
Lautaro Orellano <Lautaa123@gmail.com>
2025-09-21 01:59:52 -0300

    chore: move the logic

--- src/com/loganalizyr/processor/LogProcessor.java
+++ src/com/loganalizyr/processor/LogProcessor.java
@@ -2,39 +2,13 @@ package com.loganalizyr.processor;
 
 import com.loganalizyr.model.LogEntry;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LogProcessor {
 
-    private String keyword;
 
-    public LogProcessor(String keyword) {
-        this.keyword = keyword;
-    }
-
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    /**
-     * filtra los logs que contienen la palabra clave
-     * @param logs lista de logs a filtrar
-     * @return lista de logs filtradros
-     */
-    public List<LogEntry> filterLogs(List<LogEntry> logs) {
-        List<LogEntry> filterLogs = new ArrayList<>();
-        for (LogEntry log : logs) {
-            if (log.getMessage().contains(keyword) ||
-                log.getLevel().contains(keyword) ||
-                log.getTimestamp().contains(keyword)) {
-                filterLogs.add(log);
-            }
-        }
-        return filterLogs;
-    }
 }
