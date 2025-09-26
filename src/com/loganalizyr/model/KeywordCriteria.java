f114a4a70bc87de1b41171339918e973d44bca64
Lautaro Orellano <Lautaa123@gmail.com>
2025-09-26 00:45:04 -0300

    feat: finalizacion de metodo contenedor de logica de patron de busqueda

--- src/com/loganalizyr/model/KeywordCriteria.java
+++ src/com/loganalizyr/model/KeywordCriteria.java
@@ -1,14 +1,27 @@
 package com.loganalizyr.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class KeywordCriteria {
+    private String keyword;
     private boolean useRegex;
     private boolean useLiteral;
     private boolean caseSensitive;
 
-    public KeywordCriteria(boolean useRegex, boolean useLiteral, boolean caseSensitive) {
+    public KeywordCriteria(String keyword, boolean useRegex, boolean useLiteral, boolean caseSensitive) {
+        this.keyword = keyword;
         this.useRegex = useRegex;
         this.useLiteral = useLiteral;
         this.caseSensitive = caseSensitive;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
     }
 
     public boolean isUseRegex() {
@@ -35,5 +48,16 @@ public class KeywordCriteria {
         this.caseSensitive = caseSensitive;
     }
 
-
+    public boolean matches(String logMessage) {
+        if (logMessage != null && !logMessage.isEmpty()) {
+            if (isUseLiteral()) {
+                return logMessage.contains(keyword);
+            } else if (isUseRegex()) {
+                Pattern pattern = Pattern.compile(keyword);
+                Matcher matcher = pattern.matcher(logMessage);
+                return matcher.find();
+            }
+        }
+        return false;
+    }
 }
