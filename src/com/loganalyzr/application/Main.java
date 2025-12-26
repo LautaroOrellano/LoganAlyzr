247ec43054b744e6ba71b457f7d9107e03283a09
Lautaro Orellano <lautaa123@gmail.com>
2025-12-26 20:30:49 -0300

    refactor(Main): test para lectura de jsonl

--- src/com/loganalyzr/application/Main.java
+++ src/com/loganalyzr/application/Main.java
@@ -1,9 +1,25 @@
 package com.loganalyzr.application;
 
+import com.loganalyzr.core.ports.LogSource;
+import com.loganalyzr.infrastructure.persistence.JsonLogReader;
+import com.loganalyzr.infrastructure.persistence.SmartFileReader;
+
 public class Main {
     public static void main(String[] args) {
+        //String path = "logs.txt";
+        String path = "logs.jsonl";
 
-        new Agent().run();
+        //String logRegex = "(?<level>\\w+)\\s+\\[(?<date>.*?)\\]\\s+-\\s+(?<message>.*)";
+        //String dateFormat = "yyyy-MM-dd HH:mm:ss";
+
+        //SmartFileReader fileReader = new SmartFileReader(path, logRegex, dateFormat);
+
+        LogSource fileReader = new JsonLogReader(path);
+
+        Agent agent = new Agent(fileReader);
+
+        System.out.println("Iniciando agent ...");
+        agent.run();
     }
 
 }
