fd3d792fb015dcf90060aea2329e15b84ffa8bf0
Lautaro Orellano <Lautaa123@gmail.com>
2025-12-30 00:44:27 -0300

    refactor: logica de implementacion de hilos asincronicos

--- src/com/loganalyzr/application/pipeline/LogPipeline.java
+++ src/com/loganalyzr/application/pipeline/LogPipeline.java
@@ -5,6 +5,7 @@ import com.loganalyzr.core.ports.LogSource;
 import com.loganalyzr.core.ports.ReportPublisher;
 import com.loganalyzr.core.service.RuleEngine;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
@@ -23,10 +24,29 @@ public class LogPipeline {
         this.queue = new LinkedBlockingDeque<>(1000);
     }
 
-    public void runIngestion() {
+    public void start() {
+        isRunning = true;
+
+        Thread ingestorThread = new Thread(this ::runIngestion);
+        Thread workerThread = new Thread(this::runProcessing);
+
+        ingestorThread.start();
+        workerThread.start();
+
+        System.out.println("Pipeline Asíncrono INICIADO.");
+    }
+
+    private void runIngestion() {
         try {
             while (isRunning) {
+                List<LogEvent> logs = logSource.fetchNewLogs();
+                if (logs.isEmpty()) {
+                    Thread.sleep(100);
+                }
 
+                for (LogEvent log : logs) {
+                    queue.put(log);
+                }
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -34,7 +54,24 @@ public class LogPipeline {
         } catch (Exception e) {
             System.err.println("Error en ingestor: " + e.getMessage());
         }
+    }
 
+    private void runProcessing() {
+        try {
+            while (isRunning || !queue.isEmpty()) {
+
+                LogEvent log = queue.take();
+
+                if (ruleEngine.matches(log)){
+                    publisher.publish(List.of(log));
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Worker detenido.");
+        } catch (Exception e) {
+            System.err.println("Error en Worker: " + e.getMessage());
+        }
     }
 
 }
